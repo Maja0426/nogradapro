44afa49fac8dd0ba7cffcd68db1759b4a5816583
Majoros Tamás <tmsmajoros@gmail.com>
2019-02-13 14:47:36 +0100

    Added DB (mongoDB), added show page, index/new/create/show ROUTE

--- app.js
+++ app.js
@@ -1,29 +1,104 @@
 var express = require('express');
 var methodOverride = require('method-override');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-  res.redirect('home');
+app.locals.moment = require('moment');
+mongoose.connect('mongodb://localhost:27017/ads_project_02', {
+  useNewUrlParser: true
+});
+mongoose.set('useFindAndModify', false);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// SCHEMA SETUP
+var adsSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  mainCategory: String,
+  category: String,
+  price: String,
+  city: String,
+  phone: String,
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-app.get('/home', function(req, res) {
-  res.render('index');
+var Ads = mongoose.model('Ads', adsSchema);
+
+app.get('/', function (req, res) {
+  res.redirect('/ads');
 });
 
-app.get('/login', function(req, res) {
+// INDEX PAGE, LIST ALL ADS
+app.get('/ads', function (req, res) {
+  Ads.find({}, function (err, allAds) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(allAds);
+      res.render('ads/index', {
+        ads: allAds
+      });
+    }
+  })
+});
+
+// NEW ADS PAGE
+app.get('/ads/new', function (req, res) {
+  res.render('ads/new');
+});
+
+// CREATE NEW ADS
+app.post('/ads', function (req, res) {
+  var newAds = req.body.ads;
+  Ads.create(newAds, function (err, createdAds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/ads');
+    }
+  })
+});
+
+// SHOW PAGE
+app.get('/ads/:id', function (req, res) {
+  Ads.findById(req.params.id, function(err, foundAd) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('ads/show', {
+        ad: foundAd
+      });
+    }
+  })
+});
+
+
+
+
+app.get('/login', function (req, res) {
   res.render('login');
 })
 
-app.get('/register', function(req, res) {
+app.get('/register', function (req, res) {
   res.render('register');
 })
 
-app.listen(PORT, function() {
+// 404 ERROR PAGE
+app.get('*', function (req, res) {
+  res.render('404');
+});
+
+app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
 })
